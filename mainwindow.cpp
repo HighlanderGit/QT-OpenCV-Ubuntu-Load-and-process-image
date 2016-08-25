dd405737c88fd09e9eb70197569d3b77ae90656f
HighlanderGit <erivel@estudiantec.cr>
2016-08-25 15:21:38 -0600

    I have added documentation to the code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -32,7 +32,7 @@ MainWindow::MainWindow(QWidget *parent) :
     //***********************************************
 
 
-    cv::Mat frame = cv::imread("vlcsnap-00060.png");
+    cv::Mat frame = cv::imread("soccer01.png");
 
     if(!frame.data)
     {
@@ -46,40 +46,52 @@ MainWindow::MainWindow(QWidget *parent) :
     //-----------------------------------------------------------------------
     //-----------------------------------------------------------------------
 
-    //Las funciones de procesameinto de imagenes deben de ir aqui, la imagend de entrada es en este punto BGR
-    //add the code here
+    //From this point backward the image should be in the format BGR
+
+    //Section 1  --------Extract hue plane
 
 
     //Conver image to HSV
     cv::Mat frame_hsv;
     cv::cvtColor(frame, frame_hsv, CV_BGR2HSV);
 
-    //creates an array of planes
+    //creates an array of planes in order to split the planes
     std::vector<cv::Mat> ColorPlanes;
 
     //Split the images into the different planes
     cv::split(frame_hsv,ColorPlanes);
 
 
-    //Threshold of the image with the hue plane
-    //double thresh =150;
+    //Saves hue plane on the image hue_plane
+    cv::Mat hue_plane = ColorPlanes[0].clone();
+
+    //Section 2  --------Extract the soccer field
+
+    //Section 2.a threshold of the image
+    //Threshold ranges and the image for the threshold from the hue_plane
     double maxValue =75;
     double minValue =26;
     cv::Mat threshold_image;
 
-    cv::inRange(ColorPlanes[0],minValue, maxValue, threshold_image);
+    cv::inRange(hue_plane, minValue, maxValue, threshold_image);
 
-         //Create vector of contours
+    //Section 2.b threshold image filling the holes
+
+    //Create vector of contours in order to find all the blobs
     std::vector<std::vector<cv::Point> > contours;
 
+    //Creates a temporary image or buffer that is going to contain all the contours, for some reason the findContours image modifies the image this is why this image is needed.
     cv::Mat Contour_image = threshold_image.clone();
 
+    //Finds all the blobs or particles
     findContours(Contour_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE , cv::Point(0,0));
 
+    //Calculates all the Moments, the goal is to calculate the centroid in order to fill all the blobs of the image
     std::vector<cv::Moments> mu(contours.size() );
     for( int i = 0; i < contours.size(); i++ )
      { mu[i] = moments( contours[i], false ); }
 
+    //Calculates all the centroids with the moments information,the goal is to fill all the holes of the image
     std::vector<cv::Point2f> mc (contours.size());
     for(int i =0 ; i< contours.size(); i++){
 
@@ -87,12 +99,15 @@ MainWindow::MainWindow(QWidget *parent) :
 
     }
 
+    //Fill all the holes wiht the centroid information that was previously calculated
     for(int i=0; i < mc.size(); i++){
 
         //floodFill(threshold_image, mc[i], cv::Scalar(255));
         cv::drawContours(threshold_image, contours, i, cv::Scalar(255), CV_FILLED, 8   );
 
     }
+
+    //Section 2.c threshold image, holes filled remove small contours
 
     cv::bitwise_not(threshold_image,threshold_image);
 
@@ -107,33 +122,41 @@ MainWindow::MainWindow(QWidget *parent) :
 
     }
 
+    //Section 3 generate a new image from the hue_plane applying the threshold image as a mask
+
     cv::bitwise_not(threshold_image,threshold_image);
 
     cv::Mat dst1;
-    ColorPlanes[0].copyTo(dst1,threshold_image);
+    hue_plane.copyTo(dst1,threshold_image);
 
     cv::Mat dst2;
     cv::bitwise_not(threshold_image,threshold_image);
     dst1.copyTo(dst2,threshold_image);
 
-    ColorPlanes[0] = dst1 + dst2;
+    hue_plane = dst1 + dst2;
 
-    cv::inRange(ColorPlanes[0],150, 255, ColorPlanes[0]);
+    //Section 4 identify players
+
+    //Threshold of the hue_plane the brightes objects belongs to the color red at least in this case
+    cv::inRange(hue_plane,150, 255, hue_plane);
 
     int erosion_size =3;
     cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1), cv::Point(erosion_size, erosion_size) );
 
-    cv::dilate(ColorPlanes[0], ColorPlanes[0], element);
+    //Dilates the image we do this in case the player has been split into two sections
+    cv::dilate(hue_plane, hue_plane, element);
 
-    findContours(ColorPlanes[0], contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE , cv::Point(0,0));
+    //finds all the blobs and particles in order to classify as players
+    findContours(hue_plane, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE , cv::Point(0,0));
 
+    //Section 5 Generates the bounding rectacles for the blob and particles in order to draw on top of the original frame
     cv::Rect boundRect;
     std::vector<cv::Point> contours_poly ;
 
+    //Calculates the areas of the blobs and chooses if the blob is gargabe or if it a player
     for (int i = 0; i< contours.size(); i++){
 
-        //qDebug() << "Area " << i <<" = "<< cv::contourArea(contours[i]) ;
-        if(cv::contourArea(contours[i]) > 60){
+        if(cv::contourArea(contours[i]) > 60 && cv::contourArea(contours[i]) < 300 ){
 
             cv::approxPolyDP( cv::Mat(contours[i]), contours_poly, 3, true );
             boundRect = cv::boundingRect( cv::Mat(contours_poly ));
@@ -143,113 +166,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     }
 
-
-    //-----------------------------------------------------------------------
-    //-----------------------------------------------------------------------
-    //-----------------------------------------------------------------------
-
-    qDebug() <<"Array size" << contours.size();
-/*
-    for( int i = 0; i < contours.size(); i++ )
-       { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-         boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-         minEnclosingCircle( contours_poly[i], center[i], radius[i] );
-       }
-
-
-
-    //cv::contourArea(contours,)
-
-    /*
-
-    std::vector<cv::Moments> mu(contours.size() );
-    for( int i = 0; i < contours.size(); i++ )
-     { mu[i] = moments( contours[i], false ); }
-
-    std::vector<cv::Point2f> mc (contours.size());
-    for(int i =0 ; i< contours.size(); i++){
-
-        mc[i] = cv::Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-
-    }
-
-    */
-
-    /*/------------------------------------------------/*
-    Contour_image = threshold_image.clone();
-
-    findContours(Contour_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE , cv::Point(0,0));
-
-        for( int i = 0; i < contours.size(); i++ )
-     { mu[i] = moments( contours[i], false ); }
-
-     for(int i =0 ; i< contours.size(); i++){
-
-        mc[i] = cv::Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-
-    }
-
-     if ( !contours_all.empty()) {
-         for (size_t i=0; i<contours.size(); ++i) {
-             contour_area = contourArea(contours_all[i]) ;
-             if ( contour_area < threshold)
-                 small_blobs.push_back(i);
-         }
-     }
-
-
-    for(int i=0; i < mc.size(); i++){
-
-        //floodFill(threshold_image, mc[i], cv::Scalar(255));
-        cv::drawContours(threshold_image, contours, i, cv::Scalar(10), CV_FILLED, 8   );
-
-    }
-
-    */
-
-    //cv::floodFill(threshold_image, cv::Point(0,0), cv::Scalar(0));
-
-    //cv::bitwise_not(threshold_image,threshold_image);
-
-    /*
-     *************Example to use threshold and draw a circle on an image
-    cv::Mat frame_hsv;
-    cv::cvtColor(frame, frame_hsv, CV_BGR2HSV);
-
-    cv::split(frame,ColorPlanes);
-    //cv::equalizeHist(ColorPlanes[0], ColorPlanes[0]);
-    //cv::equalizeHist(ColorPlanes[1], ColorPlanes[1]);
-    //cv::equalizeHist(ColorPlanes[2], ColorPlanes[2]);
-
-    cv::Mat upper_red_hue_range;
-    cv::Mat lower_red_hue_range;
-
-    cv::inRange(frame_hsv, cv::Scalar(0, 100, 100), cv::Scalar(10, 255, 255), lower_red_hue_range);
-    cv::inRange(frame_hsv, cv::Scalar(160, 100, 100), cv::Scalar(179, 255, 255), upper_red_hue_range);
-
-    cv::Mat red_hue_image;
-    cv::addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
-
-    cv::GaussianBlur(red_hue_image, red_hue_image, cv::Size(9, 9), 2, 2);
-
-    // Use the Hough transform to detect circles in the combined threshold image
-    std::vector<cv::Vec3f> circles;
-    cv::HoughCircles(red_hue_image, circles, CV_HOUGH_GRADIENT, 1, red_hue_image.rows/8, 100, 20, 0, 0);
-
-    // Loop over all detected circles and outline them on the original image
-    if(circles.size() == 0) std::exit(-1);
-        for(size_t current_circle = 0; current_circle < circles.size(); ++current_circle) {
-            cv::Point center(std::ceil(circles[current_circle][0]), std::ceil(circles[current_circle][1]));
-            int radius = std::ceil(circles[current_circle][2]);
-
-            cv::circle(frame, center, radius, cv::Scalar(0, 255, 0), 5);
-        }
-
-    */
-    //cv::merge(ColorPlanes,frame);
-
-    //
-    //La imagen se espera que este en BGR en este punto para que el video pueda ser reproducido
+    //------------------------------------------------------------
     //------------------------------------------------------------
     //------------------------------------------------------------
 
